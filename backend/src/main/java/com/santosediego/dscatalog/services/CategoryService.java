4ec71c7f0744d9d7db372efbf78d8306972885ab
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-10-02 14:38:30 -0300

    Database seeding

--- backend/src/main/java/com/santosediego/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/santosediego/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.santosediego.dscatalog.entities.Category;
 import com.santosediego.dscatalog.repositories.CategoryRepository;
@@ -13,8 +14,12 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll(){
+
+	@Transactional(readOnly = true)
+	public List<Category> findAll() {
 		return repository.findAll();
 	}
 }
+//@Transactional para garantir a integridade das transações;
+//(readOnly = true) evita que faça lokin no bd, likin trava o bd, é sempre
+//bom colocar isso quando for somente leitura.
