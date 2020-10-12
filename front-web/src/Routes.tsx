84cf0c36e92599ad45c58538997dd772caf3ce11
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-10-12 14:44:32 -0300

    Product details and componet price

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -4,6 +4,7 @@ import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
 import Home from './pages/Home';
 import Navbar from './core/components/Navbar';
+import ProductDetails from './pages/Catalog/components/ProductDetails'
 
 const Routes = () => (
     <BrowserRouter>
@@ -12,8 +13,11 @@ const Routes = () => (
             <Route path="/" exact>
                 <Home />
             </Route>
-            <Route path="/catalog">
+            <Route path="/products" exact>
                 <Catalog />
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetails />
             </Route>
             <Route path="/admin">
                 <Admin />
