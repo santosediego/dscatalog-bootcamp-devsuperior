9368bdf48557f20d83606e2f6b0ff17566568001
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-10-19 07:11:17 -0300

    Pagination

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -4,17 +4,19 @@ import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
 import { ProductsResponse } from 'core/types/Product';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
+import Pagination from 'core/components/Pagination';
 import './styles.scss'
 
 const Catalog = () => {
     
     const [ productsResponse, setProductsResponse ] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
+    const [ activePage, setActivePage]= useState(0);
 
     useEffect(() => {
 
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
 
@@ -26,7 +28,7 @@ const Catalog = () => {
                 // Finaliza o loader;
                 setIsLoading(false);
             })
-    }, []);
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
@@ -42,6 +44,13 @@ const Catalog = () => {
                     ))
                 )}
             </div>
+            {productsResponse && (
+                <Pagination 
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />
+            )}
         </div>
     );
 }
