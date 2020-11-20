b2d3cd3fa5580b481223cc8fcc855a1cd9b93b61
santosediego <diegoeustaquiodossantos@hotmail.com>
2020-11-20 06:24:49 -0300

    Create auth/login module

---
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -0,0 +1,56 @@
+import ButtonIcon from 'core/components/ButtonIcon';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import AuthCard from '../Card';
+import './styles.scss';
+
+type FormData = {
+    email: string;
+    password: string;
+}
+
+const Login = () => {
+
+    const { register, handleSubmit } = useForm<FormData>(); // initialize the hook
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+        // Chamar a api de autenticação;
+    }
+
+    return(
+        <AuthCard title="login" >
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                <input 
+                    type="email" 
+                    className="form-control input-base margin-botton-30"
+                    placeholder="Email"
+                    name="email"
+                    ref={register}
+                />
+                <input 
+                    type="password" 
+                    className="form-control input-base"
+                    placeholder="Senha"
+                    name="password"
+                    ref={register}
+                />
+                <Link to="/admin/auth/recover" className="login-link-recover">
+                    Esqueci a senha?
+                </Link>
+                <div className="login-submit">
+                    <ButtonIcon text="logar"/>
+                </div>
+                <div className="text-center">
+                    <span className="not-registered">Não tem Cadastro?</span>
+                    <Link to="/admin/auth/register" className="login-link-register">
+                        CADASTRAR
+                    </Link>
+                </div>
+            </form>
+        </AuthCard>
+    )
+};
+
+export default Login;
