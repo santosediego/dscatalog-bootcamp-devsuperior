bdbb94e3e08dac864142e9b862fe94373c45b994
santosediego <diegoeustaquiodossantos@hotmail.com>
2020-11-26 22:20:06 -0300

    Product listing and pagination

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import Pagination from 'core/components/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from '../Card';
 
 const List = () => {
 
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
     const history = useHistory();
+
+    console.log(productsResponse);
+
+    useEffect(() => {
+
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+
+        // Inicia o loader;
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                // Finaliza o loader;
+                setIsLoading(false);
+            })
+    }, [activePage]);
+
     const handleCreate = () => {
         history.push('/admin/products/create');
     }
 
-    return(
+    return (
         <div className="admin-products-list">
             <button className="btn btn-primary brn-lg" onClick={handleCreate}>
                 ADICIONAR
             </button>
             <div className="admin-list-container">
-                <Card/>
-                <Card/>
-                <Card/>
+                {productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id} />
+                ))}
+                {productsResponse && (
+                    <Pagination
+                        totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+                )}
             </div>
         </div>
     );
