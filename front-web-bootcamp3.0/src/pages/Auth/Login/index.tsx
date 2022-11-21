a594576c58ed092dec116c96bdf14cf8dcda2f40
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-11-21 10:56:41 -0300

    Login form validations

--- front-web-bootcamp3.0/src/pages/Auth/Login/index.tsx
+++ front-web-bootcamp3.0/src/pages/Auth/Login/index.tsx
@@ -1,8 +1,9 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
-import './styles.css';
 import { requestBackendLogin } from 'util/requests';
+import './styles.css';
 
 type FormData = {
     username: string,
@@ -11,14 +12,18 @@ type FormData = {
 
 const Login = () => {
 
-    const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
     const onSubmit = (formData: FormData) => {
+        setHasError(false);
+
         requestBackendLogin(formData)
             .then(response => {
                 console.log('sucesso', response);
             })
             .catch(error => {
+                setHasError(true);
                 console.log('deu ruim', error);
             })
     }
@@ -26,24 +31,49 @@ const Login = () => {
     return (
         <div className="base-card login-card">
             <h1>LOGIN</h1>
+
+            {hasError && (
+                <div className="alert alert-danger">
+                    E-mail ou senha invalidos!
+                </div>
+            )}
+
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                     <input
-                        {...register("username")}
+                        {...register("username", {
+                            required: { value: true, message: "Campo obrigatório!" },
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Email inválido"
+                            }
+                        })}
                         type="text"
-                        className="form-control base-input"
+                        className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
                         placeholder="Email"
                         name="username"
                     />
+                    {errors.username && (
+                        <div className="invalid-feedback d-block">
+                            {errors.username.message}
+                        </div>
+                    )}
                 </div>
                 <div className="mb-2">
                     <input
-                        {...register("password")}
+                        {...register("password", {
+                            required: { value: true, message: "Campo obrigatório!" }
+                        })}
                         type="password"
-                        className="form-control base-input "
+                        className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
                         placeholder="Password"
                         name="password"
                     />
+                    {errors.password && (
+                        <div className="invalid-feedback d-block">
+                            {errors.password.message}
+                        </div>
+                    )}
                 </div>
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha
