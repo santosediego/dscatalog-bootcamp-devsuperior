7a02444ad9fcd6f46d278391b49d97f2a719a976
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-12-02 14:08:58 -0300

    Bolci describe and test suits

--- front-web-bootcamp3.0/src/util/__tests__/formatters.spec.ts
+++ front-web-bootcamp3.0/src/util/__tests__/formatters.spec.ts
@@ -1,7 +1,25 @@
-import { formatPrice } from "util/formatters";
+import { formatPrice } from 'util/formatters';
 
-test('formatPric should format number pt-BR when given 10.1', () => {
+describe('formatePrice for positive numbers', () => {
+    test('should format number pt-BR when given 10.1', () => {
+        const result = formatPrice(10.1);
+        expect(result).toEqual('10,10');
+    });
 
-    const result = formatPrice(10.1);
-    expect(result).toEqual("10,10");
-})
+    test('should format number pt-BR when given 0.1', () => {
+        const result = formatPrice(0.1);
+        expect(result).toEqual('0,10');
+    });
+});
+
+describe('formatePrice for negative numbers', () => {
+    test('should format number pt-BR when given 0', () => {
+        const result = formatPrice(0);
+        expect(result).toEqual('0,00');
+    });
+
+    test('should format number pt-BR when given -5.1', () => {
+        const result = formatPrice(-5.1);
+        expect(result).toEqual('-5,10');
+    });
+});
