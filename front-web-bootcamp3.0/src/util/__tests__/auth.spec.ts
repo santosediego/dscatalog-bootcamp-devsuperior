426902ee5ed92692635915ca54daef456cc6f486
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-12-02 14:53:51 -0300

    Mocing function with spyOn - hasAnyRole

---
+++ front-web-bootcamp3.0/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,22 @@
+import { hasAnyRole } from '../auth';
+// referência para o token para fazer um mock
+import * as TokenModule from '../token';
+
+describe('hasAnyRole tests', () => {
+    test('should return true when empty list', () => {
+        const result = hasAnyRole(([]));
+        expect(result).toEqual(true);
+    });
+
+    test('should return true when user has given role', () => {
+
+        jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: '',
+            authorities: ['ROLE_ADMIN', 'ROLE_OPERATOR'],
+        });
+
+        const result = hasAnyRole((['ROLE_ADMIN']));
+        expect(result).toEqual(true);
+    });
+});
--- front-web-bootcamp3.0/src/util/__tests__/formatters.spec.ts
+++ front-web-bootcamp3.0/src/util/__tests__/formatters.spec.ts
@@ -1,25 +1,13 @@
 import { formatPrice } from 'util/formatters';
 
-describe('formatePrice for positive numbers', () => {
+describe('formatePrice tests', () => {
     test('should format number pt-BR when given 10.1', () => {
         const result = formatPrice(10.1);
         expect(result).toEqual('10,10');
     });
 
-    test('should format number pt-BR when given 0.1', () => {
-        const result = formatPrice(0.1);
-        expect(result).toEqual('0,10');
-    });
-});
-
-describe('formatePrice for negative numbers', () => {
     test('should format number pt-BR when given 0', () => {
         const result = formatPrice(0);
         expect(result).toEqual('0,00');
-    });
-
-    test('should format number pt-BR when given -5.1', () => {
-        const result = formatPrice(-5.1);
-        expect(result).toEqual('-5,10');
     });
 });
